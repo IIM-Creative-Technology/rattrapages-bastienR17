c68e64d8fa48e497fa0d8be7309f784b37a8f7c8
“bastienr17” <bastienroc@free.fr>
2023-06-21 20:48:01 +0200

    add image & section 2 💄

--- 03_Tailwind/tailwind.config.js
+++ 03_Tailwind/tailwind.config.js
@@ -4,18 +4,23 @@ module.exports = {
     './dist/*.{html, js}',
   ],
   theme: {
-    colors: {
-      'Primary': '#B2D234',
-      'White': '#FFFFFF',
-    },
 
     fontFamily: {
       'poppins': ['Poppins', 'sans-serif'],
     },
 
     extend: {
+      colors: {
+        'Primary': '#B2D234',
+        'Secondary': '#60A0D8',
+        'White': '#FFFFFF',
+        'Text': '#606060',
+      },
+      
       backgroundImage : {
-        'fond' : "url('/src/images/pattern2.jpg')",
+        'fond' : "url('/src/images/pattern2.png')",
+        'salade' : " url('/src/images/salade.png')",
+
       }
     },
   },
